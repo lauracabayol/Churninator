e5a4cd0ca6ec9e1a04c492a5d57c380eea335fb8
Laura Cabayol Garcia <lauracabayol@lauras-macbook-pro.home>
2024-08-05 09:25:07 +0200

    add plots that were in DataCleaner before

--- Churninator/utils/plots.py
+++ Churninator/utils/plots.py
@@ -30,3 +30,42 @@ def plot_feature_importances_seaborn(feature_importance_dict):
     sns.barplot(x='Importance', y='Feature', data=feature_importance_df, palette='viridis')
     plt.title('Feature Importances')
     plt.show()
+
+
+def plot_correlation_matrix(df, columns):
+    """
+    Plots the correlation matrix for the specified columns in the DataFrame.
+
+    Parameters:
+    columns (list): List of column names to include in the correlation matrix.
+    """
+
+    # Select the specified columns
+    df_selected = df[columns]
+    
+    # Calculate the correlation matrix
+    corr_matrix = df_selected.corr()
+    
+    # Create a heatmap of the correlation matrix
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', center=0, linewidths=0.5, linecolor='black')
+    plt.title('Correlation Matrix')
+    plt.show()
+
+def plot_correlation2exit(df):
+    """
+    Plots the correlation of features with the target variable 'Exited'.
+    """
+
+    # Calculate correlations with the target variable
+    correlations = df.corr()['Exited'].drop('Exited')
+    
+    # Plot the correlations
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=correlations.index, y=correlations.values, palette='coolwarm')
+    plt.title('Correlation with Exit')
+    plt.xlabel('Feature')
+    plt.ylabel('Correlation')
+    plt.xticks(rotation=45)
+    plt.show()
+
